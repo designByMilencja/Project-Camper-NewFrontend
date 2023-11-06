6c7dbca2a4ac49894564c0d5e770ff942cb77e6b
Milena Pieńkosz <designbymilencja@gmail.com>
2023-11-06 15:20:43 +0100

    feat(AddView): created select field for choose country and category

---
+++ src/components/views/AddView/AddPaymentView/SelectCategory/SelectCategory.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {CategoryEntity} from "types";
+
+interface Props {
+    form: { idCategory: string }
+    categoriesData: CategoryEntity[] | null;
+    saveForm: (name: string, value: string) => void;
+}
+
+export const SelectCategory = ({categoriesData, form, saveForm}: Props) => {
+    return (<>
+        <label>Kategoria zakupu</label>
+        <select required name="category" value={form.idCategory} onChange={e => saveForm('idCategory', e.target.value)}>
+            <option>--</option>
+            {categoriesData?.map((category: CategoryEntity) => <option key={category.id} value={category.id}>{category.name}</option>) ?? []}
+        </select>
+    </>)
+}
