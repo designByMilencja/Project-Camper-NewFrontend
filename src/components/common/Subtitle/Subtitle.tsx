0127621e7ede0eb0f5c5a67e085effa83b1c5e56
Milena Pieńkosz <designbymilencja@gmail.com>
2023-11-07 15:42:42 +0100

    fix(src): removed no needed component

--- src/components/common/Subtitle/Subtitle.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Subtitle.scss";
-
-interface Props {
-    text: string;
-    color?: string;
-}
-
-export const Subtitle = ({text, color}: Props) => <h2 className="subtitle" style={{color: color}}>{text}</h2>
