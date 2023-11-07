45dadfb34a497d7608240a0f439b6a4980b7e91c
Milena Pieńkosz <designbymilencja@gmail.com>
2023-11-07 15:39:02 +0100

    fix(HeaderView): removed no needed styles

--- src/components/views/HeaderView/HeaderView.tsx
+++ src/components/views/HeaderView/HeaderView.tsx
@@ -5,14 +5,12 @@ import {Datetime} from "./DateTime/Datetime";
 export const HeaderView = () => {
     return <>
         <header>
-            <div className="header-box">
                 <div className="shadow"></div>
                 <div className="header-text">
                     <h1>Camper Cash</h1>
                     <h2>Zaplanuj wydatki w podróży</h2>
                     <Datetime/>
                 </div>
-            </div>
         </header>
     </>
 }
