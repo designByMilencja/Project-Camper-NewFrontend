86f9e7d19eb097bffc6062a86fc7bcbeff6f08c1
Milena Pieńkosz <designbymilencja@gmail.com>
2023-11-07 20:05:53 +0100

    fix(SelectCategory): typo in sentence

--- src/components/views/AddView/AddPaymentView/SelectCategory/SelectCategory.tsx
+++ src/components/views/AddView/AddPaymentView/SelectCategory/SelectCategory.tsx
@@ -11,7 +11,7 @@ export const SelectCategory = ({categoriesData, form, saveForm}: Props) => {
     return (<>
         <label>Kategoria zakupu</label>
         <select required name="category" value={form.idCategory} onChange={e => saveForm('idCategory', e.target.value)}>
-            <option>--</option>
+            <option></option>
             {categoriesData?.map((category: CategoryEntity) => <option key={category.id} value={category.id}>{category.name}</option>) ?? []}
         </select>
     </>)
